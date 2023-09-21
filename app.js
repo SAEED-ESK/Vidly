5b2b7ecd19c31655c52ed3ae67e4684f3737bf96
Saeed Eskandari <eskandary.saeed336@gmail.com>
2023-09-21 11:48:33 +0330

    first commit of the repository

---
+++ app.js
@@ -0,0 +1,12 @@
+const winston = require('winston');
+const express = require('express');
+const app = express();
+
+require('./start/logging')()
+require('./start/routes')(app)
+require('./start/db')()
+// require('./start/config')()
+require('./start/validation')()
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
