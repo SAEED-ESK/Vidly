c672de52e4b791eb6e6768395759c372ddb1bf6e
Saeed Eskandari <eskandary.saeed336@gmail.com>
2023-10-31 10:53:20 +0330

    changing some files

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const winston = require('winston');
-const express = require('express');
+const winston = require("winston");
+const express = require("express");
 const app = express();
 
-require('./start/logging')()
-require('./start/routes')(app)
-require('./start/db')()
+require("./start/logging")();
+require("./start/routes")(app);
+require("./start/db")();
 // require('./start/config')()
-require('./start/validation')()
+require("./start/validation")();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-    const token = req.header('x-auth-token');
-    if (!token) return res.status(401).send('Access Denied');
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access Denied");
 
-    try {
-        const decode = jwt.verify(token, 'mySecureKey'); // config.get('jwtPrivateKey')
-        req.user = decode
-        next()
-    } catch (error) {
-        res.status(401).send('Invalid Token')
-    }
+  try {
+    const decode = jwt.verify(token, "mySecureKey"); // config.get('jwtPrivateKey')
+    req.user = decode;
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid Token");
+  }
 }
 
-module.exports = auth
+module.exports = auth;
