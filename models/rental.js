f3f7a9008439633d3f2d5459db7fe9793c1cf0f4
Saeed Eskandari <eskandary.saeed336@gmail.com>
2023-11-01 11:22:28 +0330

    changing some files

--- models/rental.js
+++ models/rental.js
@@ -1,68 +1,77 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const Rental = mongoose.model('rental', new mongoose.Schema({
-    customer: {
-        type: new mongoose.Schema({
-            name: {
-                type: 'string',
-                required: true,
-                min: 5,
-                max: 255
-            },
-            isGold: {
-                type: 'boolean',
-                default: false,
-            },
-            phone: {
-                type: "string",
-                required: true,
-                length: 11
-            },
-        }),
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: "string",
         required: true,
-    },
-    movie: {
-        type: new mongoose.Schema({
-            title: {
-                type: 'string',
-                required: true,
-                trim: true,
-                min: 5,
-                max: 255
-            },
-            dailyRentalRate: {
-                type: Number,
-                required: true,
-                min: 0,
-                max: 255
-            }
-        }),
-        require: true
-    },
-    dateOut: {
-        type: Date,
+        min: 5,
+        max: 255,
+      },
+      isGold: {
+        type: "boolean",
+        default: false,
+      },
+      phone: {
+        type: "string",
         required: true,
-        default: Date.now(),
-    },
-    dateReturned: {
-        type: Date,
-    },
-    rentalFee: {
+        length: 11,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: "string",
+        required: true,
+        trim: true,
+        min: 5,
+        max: 255,
+      },
+      dailyRentalRate: {
         type: Number,
+        required: true,
         min: 0,
-    }
-}));
+        max: 255,
+      },
+    }),
+    require: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  },
+});
+
+rentalSchema.statics.lookup = function (customerId, movieId) {
+  return this.findOne({
+    "customer._id": customerId,
+    "movie._id": movieId,
+  });
+};
+
+const Rental = mongoose.model("rental", rentalSchema);
 
 function rentalValidation(rental) {
-    const schema = Joi.object({
-        customerId: Joi.objectId().required(),
-        movieId: Joi.objectId().required(),
-    })
-    return schema.validate(rental)
+  const schema = Joi.object({
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  });
+  return schema.validate(rental);
 }
 
 module.exports = {
-    Rental,
-    rentalValidation
-}
+  Rental,
+  rentalValidation,
+};
